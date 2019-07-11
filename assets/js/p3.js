e7ec3a5baa4109b1df44bd71364f599d2fac37fa
cjstanzhu <cjstanzhu@gmail.com>
2019-07-11 14:31:34 -0400

    edit p3.js code and index.html

--- assets/js/p3.js
+++ assets/js/p3.js
@@ -1,15 +1,50 @@
 $(document).ready(function () {
 
-    let radioInputs = $(":radio");
-    console.log(radioInputs);
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyDLiD3JSjpjHexN1pghGWNvyMlEaq5HIcY",
+    //     authDomain: "project1-db25d.firebaseapp.com",
+    //     databaseURL: "https://project1-db25d.firebaseio.com",
+    //     projectId: "project1-db25d",
+    //     storageBucket: "",
+    //     messagingSenderId: "818437551042",
+    //     appId: "1:818437551042:web:fe350cab90ffaf83"
+    // };
 
+    let database = firebase.database();
+    let playersList = database.ref("gifSelected");
+
+    playersList.on("value", function (snapshot) {
+        let gifCount = 0;
+        // console.log(child.val().gifUrlLink);
+
+        snapshot.forEach(function (child) {
+            console.log(child.val().gifUrlLink);
+            // console.log(index);
+            // $("#gif-" + (gifCount + 1)).attr("src", child.val().gifUrlLink);
+            gifCount++;
+            $("#gif-" + gifCount).attr("src", child.val().gifUrlLink);
+        });
+
+        if (gifCount === 4) {
+            console.log("all players are ready");
+        };
+    });
+
+    // playersList.on("child_added", function(snapshot) {
+    //     console.log(snapshot.val().gifUrlLink);
+
+    // });
+
+    let radioInputs = $(":radio");
+    // console.log(radioInputs);
+    
     $(".form-check-input").on("click", function () {
         let value = $(this).attr("value");
         let dataGroup = $(this).attr("data-group");
 
         for (let i = 0; i < radioInputs.length; i++) {
             if (radioInputs[i].value === value) {
-                $(radioInputs[i]).attr("disabled", "true");
+                $(radioInputs[i]).attr("disabled", true);
             };
         };
 
@@ -23,33 +58,9 @@ $(document).ready(function () {
         };
     });
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyDLiD3JSjpjHexN1pghGWNvyMlEaq5HIcY",
-        authDomain: "project1-db25d.firebaseapp.com",
-        databaseURL: "https://project1-db25d.firebaseio.com",
-        projectId: "project1-db25d",
-        storageBucket: "",
-        messagingSenderId: "818437551042",
-        appId: "1:818437551042:web:fe350cab90ffaf83"
-    };
+    $("#submit-button").on("click", function() {
 
-    let db = firebase.database();
-    let playersList = db.ref("gifSelected");
 
-    playersList.on("value", function (snapshot) {
-        let gifCount = 0;
-
-        snapshot.forEach(function (child, index) {
-            child.val();
-            console.log(child.val());
-            console.log(index);
-            $("#gif-" + (gifCount + 1)).attr("src", child.val());
-            gifCount++;
-        });
-
-        if (gifCount === 4) {
-            console.log("all players ready");
-        };
     });
 
 });
