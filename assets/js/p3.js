687f9d942aea918ac4d72b289803cc746bb405a7
Héctor Morales <hector@morales.is>
2019-07-11 17:36:33 -0400

    branch update

--- assets/js/p3.js
+++ assets/js/p3.js
@@ -3,13 +3,21 @@ $(document).ready(function () {
     let radioInputs = $(":radio");
     // console.log(radioInputs);
 
+    // playersList.on("child_added", function(snapshot) {
+    //     console.log(snapshot.val().gifUrlLink);
+
+    // });
+
+    let radioInputs = $(":radio");
+    // console.log(radioInputs);
+    
     $(".form-check-input").on("click", function () {
         let value = $(this).attr("value");
         let dataGroup = $(this).attr("data-group");
 
         for (let i = 0; i < radioInputs.length; i++) {
             if (radioInputs[i].value === value) {
-                $(radioInputs[i]).attr("disabled", "true");
+                $(radioInputs[i]).attr("disabled", true);
             };
         };
 
@@ -34,16 +42,5 @@ function getGifAnswers(){
     playersList.on("value", function (snapshot) {
         let gifCount = 0;
 
-        snapshot.forEach(function (child, index) {
-            child.val();
-            console.log(child.val());
-            console.log(index);
-            $("#gif-" + (gifCount + 1)).attr("src", child.val());
-            gifCount++;
-        });
-
-        if (gifCount === 4) {
-            console.log("all players ready");
-        };
     });
 }
