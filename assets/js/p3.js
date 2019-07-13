51e1e0500daf5c2d442bd39f1528f31ab0c01bdc
cjstanzhu <cjstanzhu@gmail.com>
2019-07-13 10:19:24 -0400

    insert playerScore code for p3.js

--- assets/js/p3.js
+++ assets/js/p3.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $(".form-check-input").on("click", function () {
         let value = $(this).attr("value");
         let dataGroup = $(this).attr("data-group");
-        console.log(dataGroup);
+        // console.log(dataGroup);
         
         for (let i = 0; i < radioInputs.length; i++) {
             if (radioInputs[i].value === value) {
@@ -57,7 +57,7 @@ $(document).ready(function() {
                     gifCount++;
                     $("#gif-" + gifCount).attr("src", child.val().gifUrlLink);
                     $("#gif-" + gifCount).attr("data-playerId", child.val().pId);
-                    console.log($("#gif-" + gifCount).attr("data-playerId"));
+                    // console.log($("#gif-" + gifCount).attr("data-playerId"));
                     $("#gif-" + gifCount).attr("data-playerName", child.val().pName);
                 };
             });
@@ -69,9 +69,9 @@ $(document).ready(function() {
     function updateScore() {
         for (let i = 0; i < radioInputs.length; i++) {
             if (radioInputs[i].checked === true) {
-                console.log($(radioInputs[i]).attr("name"));
-                console.log($(radioInputs[i]).attr("value"));
-                console.log($("#" + $(radioInputs[i]).attr("name")).attr("data-playerId"));
+                // console.log($(radioInputs[i]).attr("name"));
+                // console.log($(radioInputs[i]).attr("value"));
+                // console.log($("#" + $(radioInputs[i]).attr("name")).attr("data-playerId"));
                 
                 let result = {
                     playerId: $("#" + $(radioInputs[i]).attr("name")).attr("data-playerId"),
@@ -86,14 +86,39 @@ $(document).ready(function() {
     let resultList = database.ref("results");
     let resultsCount = 0;
 
+    let player1Score = 0;
+    let player2Score = 0;
+    let player3Score = 0;
+    let player4Score = 0;
+
     resultList.on("child_added", function(snapshot) {
         resultsCount++;
-        console.log(resultsCount);
+        // console.log(resultsCount);
+        console.log(snapshot.val().playerId);
+        console.log(snapshot.val().value);
 
+        
+        if (snapshot.val().playerId === "player1") {
+            player1Score = player1Score + parseInt(snapshot.val().value);
+        } else if (snapshot.val().playerId === "player2") {
+            player2Score = player2Score + parseInt(snapshot.val().value);
+        } else if (snapshot.val().playerId === "player3") {
+            player3Score = player3Score + parseInt(snapshot.val().value);
+        } else if (snapshot.val().playerId === "player4") {
+            player4Score = player4Score + parseInt(snapshot.val().value);
+        };
+        
         if (resultsCount === 12) {
             console.log("All players allocated gif points.");
             // proceed from page3
         };
+
+        console.log(player1Score);
+        console.log(player2Score);
+        console.log(player3Score);
+        console.log(player4Score);
+
     });
 });
+
 
